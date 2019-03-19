decd2e0c659db1c4ca233b864ebcda8f9fce4f44
Diego Martin <diegomartinrecillas@outlook.com>
2019-03-19 15:10:19 -0600

    sidebar select as functional component with hooks

--- src/components/SidebarSelects.tsx
+++ src/components/SidebarSelects.tsx
@@ -1,79 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import NetworkService from '../api/NetworkService';
 
-interface OwnProps {
+interface Props {
 	programmes?: any;
 	courses?: any;
 }
 
-class SidebarSelects extends React.Component<{}, OwnProps> {
-	constructor(props: any) {
-		super(props);
-		this.state = {
-			programmes: null,
-			courses: [],
-		};
-	}
+const SidebarSelects = (props: Props) => {
+	const [programmes, setProgrammes] = useState();
+	const [courses, setCourses] = useState([]);
 
-	public componentDidMount() {
+	useEffect(() => {
 		NetworkService.getProgrammes()
-			.then(data => {
-				this.setState({ programmes: data });
-			})
-			.catch();
-		/*NetworkService.getCourses().then(data => {
-			this.setState({ courses: data });
-			console.log(this.state.courses);
-		});*/
+			.then(data => setProgrammes(data.programmes))
+			.catch(); // TODO: handle error
+	}, []);
+
+	if (!programmes || !courses) {
+		return <div>Loading...</div>;
 	}
 
-	public render() {
-		const { programmes, courses } = this.state;
-
-		if (!programmes || !courses) {
-			return <div>Loading...</div>;
-		}
-
-		return (
-			<div className="sidebar-selects">
-				<a href="">Handpicked</a>
-				<div className="spacing spacing_sm" />
-				<a className="active" href="">
-					Latest Listings
-				</a>
-				<div className="spacing spacing_lg" />
-				<div className="text-label">PROGRAMMES</div>
-				<div className="spacing spacing_xs" />
-				<select
-					className="sidebar-selects__custom-select custom-select"
-					id="programme-select"
-				>
-					{programmes.programmes.map((programme: any, index: number) => {
+	return (
+		<div className="sidebar-selects">
+			<a href="">Handpicked</a>
+			<div className="spacing spacing_sm" />
+			<a className="active" href="">
+				Latest Listings
+			</a>
+			<div className="spacing spacing_lg" />
+			<div className="text-label">PROGRAMMES</div>
+			<div className="spacing spacing_xs" />
+			<select
+				className="sidebar-selects__custom-select custom-select"
+				id="programme-select"
+			>
+				{programmes &&
+					programmes.map((programme: any, index: number) => {
 						return (
 							<option key={index} value="">
 								{programme.title.en}
 							</option>
 						);
 					})}
-				</select>
-				<div className="spacing spacing--medium" />
-				<div className="text-label">COURSES</div>
-				<div className="spacing spacing_xs" />
-				<select
-					className="sidebar-selects__custom-select custom-select"
-					id="programme-select"
-				>
-					{courses.map((course: any, index: number) => {
+			</select>
+			<div className="spacing spacing--medium" />
+			<div className="text-label">COURSES</div>
+			<div className="spacing spacing_xs" />
+			<select
+				className="sidebar-selects__custom-select custom-select"
+				id="programme-select"
+			>
+				{courses &&
+					courses.map((course: any, index: number) => {
 						return (
 							<option key={index} value="">
 								{course.name}
 							</option>
 						);
 					})}
-				</select>
-			</div>
-		);
-	}
-}
+			</select>
+		</div>
+	);
+};
 
 export default SidebarSelects;
