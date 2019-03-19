e83fa738b641b705435551cef96d571b6edd86ca
Diego Martin <diegomartinrecillas@outlook.com>
2019-03-19 15:15:24 -0600

    importing bootstrap from the main.scss file

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,37 +1,10 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 import { AppProvider } from '../../contexts/App.context';
-import NetworkService from '../../api/NetworkService';
 import Home from '../pages/Home';
 
-import Title from '../title/Title';
-import InputTitle from '../title/InputTitle';
-import ResetTitle from '../title/ResetTitle';
-
-import 'bootstrap/dist/css/bootstrap.css';
-
 const App = () => {
-	useEffect(() => {
-		/*
-		const programmes = NetworkService.getProgrammes();
-		const courses = NetworkService.getCourses();
-
-		const programme = NetworkService.getProgrammeByCode('TIMTM');
-		const course = NetworkService.getCourseByCode('ME3502');
-
-		const books = NetworkService.searchForBooks('Astrid Lindgren');
-		const book = NetworkService.getBookById('gJkuDwAAQBAJ');
-
-		console.log(programmes.then(data => console.log(data)));
-		console.log(courses.then(data => console.log(data)));
-		console.log(programme.then(data => console.log(data)));
-		console.log(course.then(data => console.log(data)));
-		console.log(books.then(data => console.log(data)));
-		console.log(book.then(data => console.log(data)));
-		*/
-	}, []);
-
 	return (
 		<Router>
 			<AppProvider>
