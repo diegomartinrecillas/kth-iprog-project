c3c8bc38259f2652f8ae62799fd8d5d886f00921
Diego Martin <diegomartinrecillas@outlook.com>
2019-03-19 18:29:19 -0600

    moved app to the src root

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
-import { AppProvider } from '../../contexts/App.context';
-import Home from '../../pages/home/Home';
+import { AppProvider } from './contexts/App.context';
+import Home from './pages/home/Home';
 
 const App = () => {
 	return (
