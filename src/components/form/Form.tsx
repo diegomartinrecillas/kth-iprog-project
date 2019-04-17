647eb0cecd2919e4ec5443c14202d8a0bdf706b2
Diego Martin <diegomartinrecillas@outlook.com>
2019-04-17 23:39:00 +0200

    fixes

--- src/components/form/Form.tsx
+++ src/components/form/Form.tsx
@@ -43,170 +43,185 @@ const Form = (props: Props) => {
 				handleSubmit,
 				isSubmitting,
 				setFieldValue,
-			}) => (
-				<form className={styles.form} onSubmit={handleSubmit}>
-					<div className="row special-gutters">
-						<div className="col-xl-8">
-							<div className="row medium-gutters">
-								<div className="col-md-6">
-									<div className="d-flex flex-column">
-										<label htmlFor="programme" className="input-label">
-											Programme
-										</label>
-										<Typeahead
-											id="programme"
-											isLoading={programmeStatus === RequestStatus.LOADING}
-											inputProps={{ name: 'programme', required: true }}
-											labelKey="title"
-											options={programmes}
-											onChange={selected =>
-												setFieldValue(
-													'programme_code',
-													selected[0].programmeCode
-												)
-											}
-											placeholder="Search programme..."
+			}) => {
+				const {
+					author,
+					programme_code,
+					course_code,
+					price,
+					new_price,
+					personal_description,
+					description,
+					release_year,
+					title,
+				} = initialValues;
+				return (
+					<form className={styles.form} onSubmit={handleSubmit}>
+						<div className="row special-gutters">
+							<div className="col-xl-8">
+								<div className="row medium-gutters">
+									<div className="col-md-6">
+										<div className="d-flex flex-column">
+											<label htmlFor="programme" className="input-label">
+												Programme
+											</label>
+											<Typeahead
+												id="programme"
+												isLoading={programmeStatus === RequestStatus.LOADING}
+												inputProps={{ name: 'programme', required: true }}
+												labelKey="title"
+												options={programmes}
+												onChange={selected =>
+													setFieldValue(
+														'programme_code',
+														selected[0].programmeCode
+													)
+												}
+												placeholder="Search programme..."
+											/>
+										</div>
+									</div>
+
+									<div className="col-md-6">
+										<div className="spacing d-xl-none" />
+										<div className="d-flex flex-column">
+											<label htmlFor="course" className="input-label">
+												Course
+											</label>
+											<AsyncTypeahead
+												id="course"
+												isLoading={courseStatus === RequestStatus.LOADING}
+												inputProps={{ name: 'course', required: true }}
+												options={courses}
+												labelKey="title"
+												minLength={3}
+												onSearch={setCourseQuery}
+												onChange={selected =>
+													setFieldValue('course_code', selected[0].courseCode)
+												}
+												placeholder="Search course..."
+												renderMenuItemChildren={(
+													option: KthCourse,
+													_,
+													index
+												) => <div key={index}>{option.title}</div>}
+											/>
+										</div>
+									</div>
+								</div>
+
+								<div className="spacing" />
+
+								<div className="row medium-gutters">
+									<div className="col-md-6">
+										<InputField
+											label="Title"
+											name="title"
+											type="text"
+											defaultValue={title}
+											onChange={handleChange}
+										/>
+									</div>
+
+									<div className="col-md-6">
+										<div className="spacing d-xl-none" />
+										<InputField
+											label="Author"
+											name="author"
+											type="text"
+											defaultValue={author}
+											onChange={handleChange}
 										/>
 									</div>
 								</div>
 
-								<div className="col-md-6">
-									<div className="spacing d-xl-none" />
-									<div className="d-flex flex-column">
-										<label htmlFor="course" className="input-label">
-											Course
-										</label>
-										<AsyncTypeahead
-											id="course"
-											isLoading={courseStatus === RequestStatus.LOADING}
-											inputProps={{ name: 'course', required: true }}
-											options={courses}
-											labelKey="title"
-											minLength={3}
-											onSearch={setCourseQuery}
-											onChange={selected =>
-												setFieldValue('course_code', selected[0].courseCode)
-											}
-											placeholder="Search course..."
-											renderMenuItemChildren={(option: KthCourse, _, index) => (
-												<div key={index}>{option.title}</div>
-											)}
+								<div className="spacing" />
+
+								<div className="row medium-gutters">
+									<div className="col-md-6">
+										<InputField
+											label="Price"
+											name="price"
+											type="number"
+											defaultValue={price && price.toString()}
+											onChange={handleChange}
+										/>
+									</div>
+
+									<div className="col-md-6">
+										<div className="spacing d-xl-none" />
+										<InputField
+											label="New price"
+											name="new_price"
+											type="number"
+											defaultValue={new_price && new_price.toString()}
+											onChange={handleChange}
 										/>
 									</div>
 								</div>
+
+								<div className="spacing" />
+
+								<div className="row medium-gutters">
+									<div className="col-md-6">
+										<InputField
+											label="Release Year"
+											name="release_year"
+											type="number"
+											defaultValue={release_year && release_year.toString()}
+											onChange={handleChange}
+										/>
+									</div>
+								</div>
+
+								<div className="spacing" />
+
+								<InputField
+									label="Book description"
+									name="description"
+									type="textarea"
+									defaultValue={description}
+									onChange={handleChange}
+								/>
 							</div>
+							<div className="col-xl-4">
+								<div className="spacing d-xl-none" />
 
-							<div className="spacing" />
+								<InputField
+									label="Cover picture"
+									name="cover_photo"
+									type="file"
+									text={values.cover_photo && values.cover_photo.name}
+									onChange={event => {
+										console.log(event.currentTarget.files[0]);
+										setFieldValue('cover_photo', event.currentTarget.files[0]);
+									}}
+								/>
 
-							<div className="row medium-gutters">
-								<div className="col-md-6">
-									<InputField
-										label="Title"
-										name="title"
-										type="text"
-										defaultValue={initialValues.title}
-										onChange={handleChange}
-									/>
-								</div>
+								<div className="spacing" />
 
-								<div className="col-md-6">
-									<div className="spacing d-xl-none" />
-									<InputField
-										label="Author"
-										name="author"
-										type="text"
-										defaultValue={initialValues.author}
-										onChange={handleChange}
-									/>
-								</div>
+								<InputField
+									label="Book condition"
+									name="personal_description"
+									type="textarea"
+									defaultValue={personal_description}
+									onChange={handleChange}
+								/>
+
+								<div className="spacing" />
+
+								<Button
+									text={add ? 'Add book' : 'Edit book'}
+									icon={add ? 'plus' : 'edit'}
+									type="submit"
+									disabled={isSubmitting}
+								/>
+								<div className="spacing" />
 							</div>
-
-							<div className="spacing" />
-
-							<div className="row medium-gutters">
-								<div className="col-md-6">
-									<InputField
-										label="Price"
-										name="price"
-										type="number"
-										defaultValue={initialValues.price.toString()}
-										onChange={handleChange}
-									/>
-								</div>
-
-								<div className="col-md-6">
-									<div className="spacing d-xl-none" />
-									<InputField
-										label="New price"
-										name="new_price"
-										type="number"
-										defaultValue={initialValues.new_price.toString()}
-										onChange={handleChange}
-									/>
-								</div>
-							</div>
-
-							<div className="spacing" />
-
-							<div className="row medium-gutters">
-								<div className="col-md-6">
-									<InputField
-										label="Release Year"
-										name="release_year"
-										type="number"
-										defaultValue={initialValues.release_year.toString()}
-										onChange={handleChange}
-									/>
-								</div>
-							</div>
-
-							<div className="spacing" />
-
-							<InputField
-								label="Book description"
-								name="description"
-								type="textarea"
-								defaultValue={initialValues.description}
-								onChange={handleChange}
-							/>
 						</div>
-						<div className="col-xl-4">
-							<div className="spacing d-xl-none" />
-
-							<InputField
-								label="Cover picture"
-								name="cover_photo"
-								type="file"
-								text={values.cover_photo && values.cover_photo.name}
-								onChange={event => {
-									console.log(event.currentTarget.files[0]);
-									setFieldValue('cover_photo', event.currentTarget.files[0]);
-								}}
-							/>
-
-							<div className="spacing" />
-
-							<InputField
-								label="Book condition"
-								name="personal_description"
-								type="textarea"
-								defaultValue={initialValues.personal_description}
-								onChange={handleChange}
-							/>
-
-							<div className="spacing" />
-
-							<Button
-								text={add ? 'Add book' : 'Edit book'}
-								icon={add ? 'plus' : 'edit'}
-								type="submit"
-								disabled={isSubmitting}
-							/>
-							<div className="spacing" />
-						</div>
-					</div>
-				</form>
-			)}
+					</form>
+				);
+			}}
 		</Formik>
 	);
 };
