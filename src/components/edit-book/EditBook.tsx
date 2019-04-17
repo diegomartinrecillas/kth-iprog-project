db1d640dd475600cee1b3f930a2be56001865400
Marco Koivisto <marcokoivisto@me.com>
2019-04-17 13:28:27 +0200

    Last fix

--- src/components/edit-book/EditBook.tsx
+++ src/components/edit-book/EditBook.tsx
@@ -6,12 +6,14 @@ const EditBook = () => {
 	return (
 		<>
 			<div className="container">
-				<a className="text-danger font-weight-bold">
-					<i className="fas fa-trash mr-2" />
-					Remove book
-				</a>
-				<div className="spacing" />
-				<div className="text-label text-label_lg">EDIT BOOK</div>
+				<div className="d-flex">
+					<div className="text-label text-label_lg">EDIT BOOK</div>
+					<div className="spacing-h" />
+					<a className="text-danger font-weight-bold">
+						<i className="fas fa-trash mr-2" />
+						Remove book
+					</a>
+				</div>
 				<div className="spacing spacing--medium" />
 				<Form />
 			</div>
