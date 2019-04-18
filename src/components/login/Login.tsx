45d695e59e9474be9ad9f0e16b3367e8f50aa995
Marco Koivisto <marcokoivisto@me.com>
2019-04-18 19:14:24 +0200

    Nope..

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -16,7 +16,7 @@ const Login = () => {
 			appId={facebookAppId}
 			autoLoad={false}
 			callback={responseFacebook}
-			scope="public_profile,email,link,location"
+			scope="public_profile,email,user_link,user_location"
 			cssClass={styles.login}
 			textButton="Continue with Facebook"
 			icon="fab fa-facebook-square"
